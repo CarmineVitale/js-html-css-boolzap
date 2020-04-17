af1dccaa46c5add3810ace5920168eebed24f844
CarmineVitale <carmine.vitale90@gmail.com>
2020-04-17 17:47:47 +0200

    Inserita parte logica ma non funzionante

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 $(document).ready(function () {
-    
+
+
+   var clone = $('.template p').clone();
+   var nuovoMex = $('input.input');
+
+
+nuovoMex.keyup(function (e) { 
+   if (e.keyCode == 13) {
+
+   
+   var textMex = nuovoMex.val();
+   $('p.ciao').text(textMex);
+   
+   $('.chat').append(clone);
+   nuovoMex.val('');
+}
+});
 
 
 
 
 
+  
+  
+  
+  
+  
+  
+   /*$('input.input').focus(function () { 
+      
+      $('.fa-microphone').toggleClass('fa-paper-plane');
+       
+   });*/
+   
 
 
 
